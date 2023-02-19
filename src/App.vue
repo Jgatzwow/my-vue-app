1632295a55ce2276617d1c41276ed2bf8c45c850
MSokol <mihasokol1993@gmail.com>
2023-02-19 10:11:35 +0300

    created endless scroll

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,11 @@ export default {
       posts: [],
       dialogVisible: false,
       isPostLoading: false,
+      page: 1,
+      limit: 10,
+      totalPages: 10,
       selectedSort: '',
+      searchQuery: '',
       sortedOptions: [
         {value: 'title', name: 'By name'},
         {value: 'body', name: 'By description'},
@@ -31,26 +35,64 @@ export default {
     showDialog() {
       this.dialogVisible = true
     },
+    /*changePage(pageNumber) {
+      this.page = pageNumber
+    },*/
     async fetchPosts() {
       this.isPostLoading = true
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        })
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
         this.posts = response.data
       } catch (e) {
         alert('Error')
       } finally {
         this.isPostLoading = false
       }
+    },
+    async loadMorePosts() {
+      this.page += 1
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        })
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = [...this.posts, ...response.data]
+      } catch (e) {
+        alert('Error')
+      }
     }
   },
   mounted() {
     this.fetchPosts()
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts()
+      }
+    }
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(this.$refs.observer)
   },
   computed: {
     sortedPosts() {
       return [...this.posts].sort((post1, post2) => {
         return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
       })
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery))
     }
   },
   /* watch: {
@@ -60,12 +102,18 @@ export default {
        })
      }
    }*/
+  watch: {
+    /*page() {
+      this.fetchPosts()
+    }*/
+  }
 }
 </script>
 
 <template>
   <div class="app">
     <h1>Page with posts</h1>
+    <my-input placeholder="Search..." v-model="searchQuery"/>
     <div class="app__btns">
       <my-button @click="showDialog">
         Create post
@@ -79,9 +127,17 @@ export default {
     <post-list
         v-if="!isPostLoading"
         @remove="removePost"
-        :posts="sortedPosts"
+        :posts="sortedAndSearchedPosts"
     />
     <div v-else>Loading...</div>
+    <!--    <div class="page__wrapper">
+          <div @click="changePage(pageNumber)" :class="{
+           ' current-page' : page === pageNumber
+          }" class="page" :key="page" v-for="pageNumber in totalPages">
+            {{ pageNumber }}
+          </div>
+        </div>-->
+    <div ref="observer" class="observer"></div>
   </div>
 
 </template>
@@ -101,5 +157,23 @@ export default {
   display: flex;
   justify-content: space-between;
   margin: 10px 0;
+}
+
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.current-page {
+  border: 2px solid teal;
+}
+
+.observer {
+
 }
 </style>
--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="post">
     <div>
+      <div>{{ post.id }}</div>
       <div>
         <strong>{{ post.title }}</strong>
       </div>
